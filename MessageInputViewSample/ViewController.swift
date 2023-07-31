e84fd77087a509054fbda2dbe3d77e57fafe5170
佐々木　謙 <nkekisasa222@gmail.com>
2023-07-31 13:26:30 +0900

    update: ViewController2 -> ViewControllerにした

--- MessageInputViewSample/ViewController.swift
+++ MessageInputViewSample/ViewController.swift
@@ -7,125 +7,7 @@
 
 import UIKit
 
-final class ViewController: UIViewController, UITextViewDelegate {
-    
-    @IBOutlet weak var tableView: UITableView!
-    @IBOutlet weak var messageInputView: UIView!
-    @IBOutlet weak var textView: UITextView!
-    
-    private var tableViewSize: CGSize?
-    private var keyboardFrame: CGRect?
-    private var messageInputViewFrame: CGRect?
-    private let MIN_HEIGHT_MSG: CGFloat = 50
-    private let MAX_HEIGHT_MSG: CGFloat = 100
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        setUp()
-    }
-    
-    private func setUp() {
-        setInitLayoutPosition()
-        setUpNotification()
-    }
-    
-    private func setInitLayoutPosition() {
-        messageInputViewFrame = messageInputView.frame
-        tableViewSize = tableView.frame.size
-    }
-    
-    private func setUpDelegate() {
-        textView.delegate = self
-    }
-    
-    private func setUpNotification() {
-        NotificationCenter.default.addObserver(
-            self,
-            selector: #selector(keyboardWillShow(_:)),
-            name: UIResponder.keyboardWillShowNotification,
-            object: nil
-        )
-        NotificationCenter.default.addObserver(
-            self,
-            selector: #selector(keyboardWillHide(_:)),
-            name: UIResponder.keyboardWillHideNotification,
-            object: nil
-        )
-    }
-    
-    @objc private func keyboardWillShow(_ notification: Notification) {
-        guard let userInfo = notification.userInfo else {
-            return
-        }
-        guard let keyboardInfo = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else {
-            return
-        }
-        keyboardFrame = keyboardInfo.cgRectValue
-        //drawMessageInputView()
-    }
-    
-    @objc private func keyboardWillHide(_ notification: Notification) {
-        guard let messageInputViewFrame = messageInputViewFrame else {
-            return
-        }
-        guard let tableViewSize = tableViewSize else {
-            return
-        }
-        messageInputView.frame = messageInputViewFrame
-        tableView.frame.size = tableViewSize
-    }
-    
-    private func drawMessageInputView() {
-        let statusBarHeight = UIApplication.shared.statusBarFrame.height
-        let screenSize = UIScreen.main.bounds.size
-        var height = textView.sizeThatFits(CGSize(
-            width: textView.frame.size.width,
-            height: CGFloat.greatestFiniteMagnitude
-        )).height
-        
-        if height < MIN_HEIGHT_MSG {
-            height = MIN_HEIGHT_MSG
-        } else if MAX_HEIGHT_MSG < height {
-            height = MAX_HEIGHT_MSG
-        }
-        
-        print("kenken", textView.frame.height)
-        print("kenken", textView.contentSize.height)
-        
-        if let keyboardFrame = keyboardFrame {
-            textView.frame = CGRect(
-                x: textView.frame.origin.x,
-                y: textView.frame.origin.y,
-                width: textView.frame.width,
-                height: height
-            )
-            messageInputView.frame = CGRect(
-                x: messageInputView.frame.origin.x,
-                y: screenSize.height - keyboardFrame.size.height - messageInputView.frame.height,
-                width: messageInputView.frame.width,
-                height: height
-            )
-            tableView.frame.size = CGSize(
-                width: tableView.frame.width,
-                height: screenSize.height - (statusBarHeight + keyboardFrame.height)
-            )
-        }
-    }
-    
-    func textViewDidChange(_ textView: UITextView) {
-        drawMessageInputView()
-    }
-    
-    @IBAction func onCamertaButtonTapped(_ sender: UIButton) {
-        print("camera.")
-    }
-    
-    @IBAction func onSendButtonTapped(_ sender: UIButton) {
-        print("send.")
-    }
-}
-
-final class ViewController2: UIViewController {
+final class ViewController: UIViewController {
     
     private var tableView = UITableView()
     private var messageInputView = UIView()
@@ -203,6 +85,7 @@ final class ViewController2: UIViewController {
         cameraButton.setImage(UIImage(systemName: "camera.fill"), for: .normal)
         cameraButton.tintColor = .black
         cameraButton.backgroundColor = .clear
+        cameraButton.addTarget(self, action: #selector(onCameraButtonTapped(_:)), for: .touchUpInside)
         messageInputView.addSubview(cameraButton)
     }
     
@@ -232,6 +115,7 @@ final class ViewController2: UIViewController {
         sendButton.setImage(UIImage(systemName: "paperplane.fill"), for: .normal)
         sendButton.tintColor = .black
         sendButton.backgroundColor = .clear
+        sendButton.addTarget(self, action: #selector(onSendButtonTapped(_:)), for: .touchUpInside)
         messageInputView.addSubview(sendButton)
     }
     
@@ -305,9 +189,17 @@ final class ViewController2: UIViewController {
             )
         }
     }
+    
+    @objc private func onCameraButtonTapped(_ sender: UIButton) {
+        print("camera.")
+    }
+    
+    @objc private func onSendButtonTapped(_ sender: UIButton) {
+        print("send.")
+    }
 }
 
-extension ViewController2: UITextViewDelegate {
+extension ViewController: UITextViewDelegate {
     
     func textViewDidChange(_ textView: UITextView) {
         drawMessageInputView()
